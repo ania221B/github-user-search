ee91277d53e4f8f529f895093bbe81e78ecb4f17
ania221B <100519699+ania221B@users.noreply.github.com>
2025-03-10 22:58:58 +0100

    dependencies update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import htmlPurge from 'vite-plugin-purgecss'
+import htmlPurge from 'vite-plugin-purgecss-updated-v5'
 
 // https://vitejs.dev/config/
 export default defineConfig({

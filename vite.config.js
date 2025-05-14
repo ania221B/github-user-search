fec57d3eb69e43c25413fc890cf3d280eed0d6ff
ania221B <100519699+ania221B@users.noreply.github.com>
2025-05-14 15:45:33 +0200

    updated config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import htmlPurge from 'vite-plugin-purgecss-updated-v5'
+import pluginPurgeCss from 'vite-plugin-purgecss-updated-v5'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/github-user-search/',
-  plugins: [react(), htmlPurge()]
+  plugins: [react(), pluginPurgeCss()]
 })

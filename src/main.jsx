8ca69ac87679a34cec4f35b873044146f0acf9b8
ania221B <100519699+ania221B@users.noreply.github.com>
2025-05-15 22:49:22 +0200

    query client and provider added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
 import AppContext from './context.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppContext>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </AppContext>
   </React.StrictMode>
 )

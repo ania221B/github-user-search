ba67f3434a515bfd7b24a53459dea6bc66d0ab34
ania221B <100519699+ania221B@users.noreply.github.com>
2025-07-10 16:48:45 +0200

    updated dependencies & replaced vite-plugin-purgecss with postcss-purgecss

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import pluginPurgeCss from 'vite-plugin-purgecss-updated-v5'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/github-user-search/',
-  plugins: [react(), pluginPurgeCss()]
+  plugins: [react()]
 })
